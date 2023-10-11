664f441b9d1658561a395251beda68977e672c8b
LENOVO <LENOVO@DESKTOP-V7BM6IG>
2023-10-11 11:06:23 +0530

    check updation

--- SampleMavenProject/src/main/java/SamplePackage/SamplePrint.java
+++ SampleMavenProject/src/main/java/SamplePackage/SamplePrint.java
@@ -2,8 +2,10 @@ package SamplePackage;
 
 public class SamplePrint {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println("Hello World ");
+		
 
 	}
 
